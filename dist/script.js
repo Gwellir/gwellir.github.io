2959861db76f46c97614a9a500f799700b9bdd12
Anna Maskaleva <effrontery89@gmail.com>
2023-07-05 12:06:11 +0300

    vote-view: added vote by groups

--- dist/script.js
+++ dist/script.js
@@ -2,28 +2,43 @@
 var VotingStage;
 (function (VotingStage) {
     VotingStage[VotingStage["SELECT_TO_VOTING"] = 0] = "SELECT_TO_VOTING";
+    VotingStage[VotingStage["VOTING"] = 1] = "VOTING";
+    VotingStage[VotingStage["WINNER"] = 2] = "WINNER";
 })(VotingStage || (VotingStage = {}));
 var VotingLocalization = {
     0: "Candidates",
-    1: "1st tour"
+    1: "Vote",
+    2: "Winner"
 };
 var Renderer = /** @class */ (function () {
     function Renderer() {
     }
     Renderer.renderCandidateView = function (candidate, options) {
         var id = "candidate".concat(candidate.index);
-        var selectionInputHTML = options.inGroup ?
-            "type=\"radio\" name=\"group".concat(candidate.group, "\" value=\"candidate").concat(candidate.index, "\"") :
+        var selectionInputHTML = options.groupName !== null ?
+            "type=\"radio\" name=\"group".concat(options.groupName, "\" value=\"candidate").concat(candidate.index, "\"") :
             "type=\"checkbox\" name=\"candidate".concat(candidate.index, "\" value=\"1\"");
-        return "<label for=\"".concat(id, "\" class=\"candidate\">\n<input ").concat(selectionInputHTML, " class=\"js-voting-item\" id=\"").concat(id, "\" data-id=\"").concat(candidate.index, "\" ").concat(options.readonly ? 'readonly' : '', " ").concat(candidate.selected ? 'checked' : '', "/>\n<div class=\"candidate-card\">\n<div class=\"candidate-card__img\"><img src=\"").concat(candidate.img, "\" alt=\"").concat(candidate.name, "\"></div>            \n            <div class=\"candidate-card__data\">\n                <span class=\"candidate-card__name\">").concat(candidate.name, "</span>\n                <span class=\"candidate-card__src\">").concat(candidate.source, "</span>            \n            </div>            \n        </div>\n        </label>");
+        return "<label for=\"".concat(id, "\" class=\"candidate\">\n<input ").concat(selectionInputHTML, " class=\"js-voting-item\" id=\"").concat(id, "\" data-id=\"").concat(candidate.index, "\" ").concat(options.readonly ? 'disabled' : '', " ").concat(candidate.selected ? 'checked' : '', "/>\n<div class=\"candidate-card\">\n<div class=\"candidate-card__img\"><img src=\"").concat(candidate.img, "\" alt=\"").concat(candidate.name, "\"></div>            \n            <div class=\"candidate-card__data\">\n                <span class=\"candidate-card__name\">").concat(candidate.name, "</span>\n                <span class=\"candidate-card__src\">").concat(candidate.source, "</span>            \n            </div>            \n        </div>\n        </label>");
     };
     Renderer.renderCandidateList = function (candidates, options) {
         return "\n      <!-- \u0421\u043F\u0438\u0441\u043E\u043A \u043A\u0430\u043D\u0434\u0438\u0434\u0430\u0442\u043E\u0432, \u043E\u0442\u043E\u0431\u0440\u0430\u0436\u0435\u043D\u0438\u0435 img, name, source, \u043F\u043E\u0441\u043B\u0435 \u0441\u0438\u0434\u0438\u043D\u0433\u0430, \u0432\u0435\u0440\u043E\u044F\u0442\u043D\u043E, seed number -->\n      <form class=\"candidates\">".concat(candidates.map(function (item) {
             return Renderer.renderCandidateView(item, {
-                inGroup: options.byGroups,
+                groupName: null,
                 readonly: options.readonly
             });
         }).join(""), "</form>\n");
+    };
+    Renderer.renderCandidateListByGroups = function (groups, options) {
+        var listHTML = "";
+        groups.forEach(function (candidates, key) {
+            listHTML += "<div class=\"candidate-group\">".concat(candidates.map(function (item) {
+                return Renderer.renderCandidateView(item, {
+                    groupName: key,
+                    readonly: options.readonly
+                });
+            }).join(""), "</div>");
+        });
+        return "<form class=\"candidates\">".concat(listHTML, "</form>");
     };
     return Renderer;
 }());
@@ -39,11 +54,42 @@ var Voting = /** @class */ (function () {
         var _this = this;
         this.fetchData()
             .then(function (response) {
-            var title = "<h1>Voting: <smaller>".concat(VotingLocalization[response.stage], "</smaller></h1>");
-            _this.el.innerHTML = title + Renderer.renderCandidateList(_this.populateCandidates(response.candidates), {
-                readonly: false,
-                byGroups: response.stage !== VotingStage.SELECT_TO_VOTING
-            });
+            var stage = VotingStage.SELECT_TO_VOTING;
+            var formattedCandidates = _this.populateCandidates(response.candidates);
+            var groups;
+            if (formattedCandidates.length !== response.candidates.length) {
+                if (formattedCandidates.length > 1 && formattedCandidates.length % 2 === 0)
+                    stage = VotingStage.VOTING;
+                else
+                    stage = VotingStage.WINNER;
+            }
+            var title = "<h1>Voting: <smaller>".concat(VotingLocalization[stage], "</smaller></h1>");
+            var html = "";
+            switch (stage) {
+                case VotingStage.SELECT_TO_VOTING: {
+                    html = Renderer.renderCandidateList(formattedCandidates, {
+                        readonly: false
+                    });
+                    break;
+                }
+                case VotingStage.WINNER: {
+                    formattedCandidates.forEach(function (candidate) {
+                        candidate.selected = true;
+                    });
+                    html = Renderer.renderCandidateList(formattedCandidates, {
+                        readonly: true
+                    });
+                    break;
+                }
+                default: {
+                    groups = _this.getCandidatesByGroups(formattedCandidates);
+                    html = Renderer.renderCandidateListByGroups(groups, {
+                        readonly: false
+                    });
+                    break;
+                }
+            }
+            _this.el.innerHTML = title + html;
             _this.readyCallback && _this.readyCallback();
         });
     };
@@ -53,15 +99,50 @@ var Voting = /** @class */ (function () {
             return response.json();
         });
     };
+    Voting.prototype.getCandidatesByGroups = function (candidates) {
+        var groups = new Map();
+        candidates.forEach(function (candidate) {
+            var key = candidate.group;
+            var oldArr = groups.get(key) || [];
+            oldArr.push(candidate);
+            groups.set(key, oldArr);
+        });
+        return groups;
+    };
     Voting.prototype.populateCandidates = function (candidates) {
-        var urlParams = new URLSearchParams(window.location.search);
-        var votes = urlParams.get('votes') || "";
+        var votes = this.getVotedIdx();
+        var available = this.getAvailableIdx();
+        if (available !== null) {
+            candidates = available.map(function (idx, index) {
+                candidates[idx].group = String(index >> 1);
+                return candidates[idx];
+            });
+        }
         return candidates.map(function (item, index) {
             item.index = index;
-            item.selected = votes[index] === "1";
+            item.selected = votes.some(function (idx) { return idx === index; });
             return item;
         });
-        return candidates;
+    };
+    Voting.prototype.getVotedIdx = function () {
+        var urlParams = new URLSearchParams(window.location.search);
+        var votes = urlParams.get('votes') || "";
+        return votes.split("")
+            .map(function (item, index) { return item === "1" ? index : -1; })
+            .filter(function (item) { return item >= 0; });
+    };
+    Voting.prototype.getAvailableIdx = function () {
+        var urlParams = new URLSearchParams(window.location.search);
+        var available = urlParams.get('available');
+        if (available === null) {
+            return null;
+        }
+        var len = available.length;
+        var res = [];
+        for (var i = 0; i < len; i += 2) {
+            res.push(Number(available[i] + available[i + 1]));
+        }
+        return res;
     };
     Voting.prototype.getSelected = function () {
         var len = document.querySelectorAll(".js-voting-item").length, res = "";
--- src/script.ts
+++ src/script.ts
@@ -15,25 +15,28 @@ interface VotingSrcData {
 }
 
 enum VotingStage {
-    SELECT_TO_VOTING = 0
+    SELECT_TO_VOTING = 0,
+    VOTING = 1,
+    WINNER = 2
 }
 
-let VotingLocalization:any = {
+let VotingLocalization: any = {
     0: "Candidates",
-    1: "1st tour"
+    1: "Vote",
+    2: "Winner"
 }
 
 class Renderer {
     static renderCandidateView(candidate: CandidateData, options: {
         readonly: boolean,
-        inGroup: boolean,
+        groupName: string | null,
     }): string {
         let id: string = `candidate${candidate.index}`;
-        let selectionInputHTML: string = options.inGroup ?
-            `type="radio" name="group${candidate.group}" value="candidate${candidate.index}"` :
+        let selectionInputHTML: string = options.groupName !== null ?
+            `type="radio" name="group${options.groupName}" value="candidate${candidate.index}"` :
             `type="checkbox" name="candidate${candidate.index}" value="1"`;
         return `<label for="${id}" class="candidate">
-<input ${selectionInputHTML} class="js-voting-item" id="${id}" data-id="${candidate.index}" ${options.readonly ? 'readonly' : ''} ${candidate.selected ? 'checked' : ''}/>
+<input ${selectionInputHTML} class="js-voting-item" id="${id}" data-id="${candidate.index}" ${options.readonly ? 'disabled' : ''} ${candidate.selected ? 'checked' : ''}/>
 <div class="candidate-card">
 <div class="candidate-card__img"><img src="${candidate.img}" alt="${candidate.name}"></div>            
             <div class="candidate-card__data">
@@ -45,25 +48,40 @@ class Renderer {
     }
 
     static renderCandidateList(candidates: CandidateData[], options: {
-        readonly: boolean,
-        byGroups: boolean
+        readonly: boolean
     }): string {
         return `
       <!-- Список кандидатов, отображение img, name, source, после сидинга, вероятно, seed number -->
       <form class="candidates">${candidates.map(item =>
             Renderer.renderCandidateView(item, {
-                inGroup: options.byGroups,
+                groupName: null,
                 readonly: options.readonly
             })
         ).join("")}</form>
 `
+    }
+
+    static renderCandidateListByGroups(groups: Map<string, CandidateData[]>, options: {
+        readonly: boolean
+    }): string {
+        let listHTML: string = "";
+        groups.forEach((candidates: CandidateData[], key: string) => {
+            listHTML += `<div class="candidate-group">${candidates.map(item =>
+                Renderer.renderCandidateView(item, {
+                    groupName: key,
+                    readonly: options.readonly
+                })
+            ).join("")}</div>`
+        })
+        return `<form class="candidates">${listHTML}</form>`
     }
 }
 
 class Voting {
     private readyCallback: Function | null = null;
 
-    constructor(private el: HTMLElement) {}
+    constructor(private el: HTMLElement) {
+    }
 
     onReady(callback: Function) {
         this.readyCallback = callback;
@@ -72,12 +90,44 @@ class Voting {
     init() {
         this.fetchData()
             .then((response: VotingSrcData) => {
-                let title:string = `<h1>Voting: <smaller>${VotingLocalization[response.stage]}</smaller></h1>`
+                let stage:VotingStage = VotingStage.SELECT_TO_VOTING;
+                let formattedCandidates: CandidateData[] = this.populateCandidates(response.candidates);
+                let groups: Map<string, CandidateData[]>;
 
-                this.el.innerHTML = title + Renderer.renderCandidateList(this.populateCandidates(response.candidates), {
-                    readonly: false,
-                    byGroups: response.stage !== VotingStage.SELECT_TO_VOTING
-                })
+                if(formattedCandidates.length !== response.candidates.length) {
+                    if (formattedCandidates.length > 1 && formattedCandidates.length % 2 === 0)
+                        stage = VotingStage.VOTING
+                    else stage = VotingStage.WINNER;
+                }
+
+                let title: string = `<h1>Voting: <smaller>${VotingLocalization[stage]}</smaller></h1>`
+                let html: string = "";
+                switch (stage) {
+                    case VotingStage.SELECT_TO_VOTING: {
+                        html = Renderer.renderCandidateList(formattedCandidates, {
+                            readonly: false
+                        });
+                        break;
+                    }
+                    case VotingStage.WINNER: {
+                        formattedCandidates.forEach(candidate =>{
+                            candidate.selected = true;
+                        })
+                        html = Renderer.renderCandidateList(formattedCandidates, {
+                            readonly: true
+                        });
+                        break;
+                    }
+                    default: {
+                        groups = this.getCandidatesByGroups(formattedCandidates);
+                        html = Renderer.renderCandidateListByGroups(groups, {
+                            readonly: false
+                        });
+                        break;
+                    }
+                }
+
+                this.el.innerHTML = title + html;
 
                 this.readyCallback && this.readyCallback();
             })
@@ -90,17 +140,59 @@ class Voting {
             })
     }
 
+    private getCandidatesByGroups(candidates: CandidateData[]): Map<string, CandidateData[]> {
+        let groups:Map<string, CandidateData[]> = new Map();
+
+        candidates.forEach(candidate => {
+            let key: string = candidate.group;
+
+            let oldArr:CandidateData[] = groups.get(key) || [];
+            oldArr.push(candidate);
+            groups.set(key, oldArr);
+        })
+        return groups;
+    }
+
     private populateCandidates(candidates: CandidateData[]): CandidateData[] {
-        const urlParams = new URLSearchParams(window.location.search);
-        const votes: string = urlParams.get('votes') as string || "";
+        let votes: number[] = this.getVotedIdx();
+        let available = this.getAvailableIdx();
+
+        if (available !== null) {
+            candidates = available.map((idx, index) => {
+                candidates[idx].group = String(index >> 1);
+                return candidates[idx];
+            })
+        }
 
         return candidates.map((item, index) => {
             item.index = index;
-            item.selected = votes[index] === "1";
+            item.selected = votes.some(idx => idx === index);
             return item;
         });
+    }
 
-        return candidates;
+    private getVotedIdx(): number[] {
+        const urlParams = new URLSearchParams(window.location.search);
+        const votes: string = urlParams.get('votes') as string || "";
+
+        return votes.split("")
+            .map((item, index) => item === "1" ? index : -1)
+            .filter(item => item >= 0)
+    }
+
+    private getAvailableIdx(): number[] | null {
+        const urlParams = new URLSearchParams(window.location.search);
+        const available: string | null = urlParams.get('available') as string;
+        if (available === null) {
+            return null
+        }
+        const len: number = available.length;
+        let res: number[] = [];
+
+        for (let i = 0; i < len; i += 2) {
+            res.push(Number(available[i] + available[i + 1]));
+        }
+        return res;
     }
 
     public getSelected(): string {
